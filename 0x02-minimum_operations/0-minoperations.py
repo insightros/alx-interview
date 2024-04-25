5202dcb7045b77b265d18574485ca4fd4070d157
xeno <xeno@xeno.0.2.15>
2024-04-25 15:25:57 +0100

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,12 +15,10 @@ def minOperations(n):
             done += clipboard
             ops_count += 2
         elif n - done > 0 and (n - done) % done == 0:
-            # copy all and paste
             clipboard = done
             done += clipboard
             ops_count += 2
         elif clipboard > 0:
-            # paste
             done += clipboard
             ops_count += 1
     return ops_count
